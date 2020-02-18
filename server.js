6ed499fdfd78b7cc7c729a18b1de7d5ec8252307
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2020-02-18 07:15:26 -0700

    consolidated code into server.js file

--- server.js
+++ server.js
@@ -4,29 +4,62 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 require('./models/Rsvp');
+const rsvpRoute = express.Router();
 
 const app = express();
+require("dotenv").config({ path: ".env" });
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-require("dotenv").config({ path: ".env" });
+const Rsvp = mongoose.model('rsvps');
+
+app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.Promise = global.Promise;
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@hugo-cluster-fla8c.mongodb.net/hugo?retryWrites=true&w=majority`, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 
-require('./routes/rsvpRoutes')(app);
+const connection = mongoose.connection;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+connection.once('open', function() {
+  console.log("MongoDB database connection established successfully");
+})
+
+rsvpRoute.route('/rsvp').post(async (req, res) => {
+  console.log(process.env.SENDGRID_API_KEY);
+  const rsvp = await Rsvp.create(req.body);
+  const msg = {
+    to: req.body.email,
+    from: { email: 'phillip@hugo-lpf.com', name: 'Hugo-LP Forums' },
+    templateId: process.env.SENDGRID_TEMPLATE_ID,
+    subject: 'Welcome to Hugo-LP Forums',
+    dynamic_template_data: {
+      name: req.body.firstName,
+    },
+  };
+
+  sgMail.send(msg);
+
+  return res.status(201).send({
+    error: false,
+    rsvp,
+  });
 });
 
+app.use('/api', rsvpRoute);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, function() {
   console.log(`Server is running on Port: ${port}`);
