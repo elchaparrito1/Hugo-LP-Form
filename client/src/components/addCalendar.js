c11615ed10fb6f3d311aaccf307e6e8b404ae7f6
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2021-02-01 18:07:59 -0700

    updated ics files

--- client/src/components/addCalendar.js
+++ client/src/components/addCalendar.js
@@ -10,7 +10,7 @@ const AddCalendar = () => (
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://www.google.com/calendar/render?action=TEMPLATE&text=HUGO+Forum+Virtual+Event&details=HUGO+Forum+is+an+invitation-only+event+that+allows+investors+to+meet+multiple+managers+in+small+LP+groups.+Our+upcoming+forum+will+be+a+virtual+event.+The+event+provides+an+efficient+platform+to+meet+other+investors+and+managers+over+two+half-day+sessions.&location=online&dates=20210330T000000Z%2F20210331T000000Z"
+              href="https://www.google.com/calendar/render?action=TEMPLATE&text=HUGO%20Forum%20-%20Virtual%20Event&dates=20210330/20210401&details=HUGO%20Forum%20-%20Virtual%20Event%0ADate%3A%20Mar%2030%2C%202021%20-%20Apr%201%2C%202021%0AVenue%3A%20Online%0AHUGO%20Forum%20is%20an%20invitation-only%20event%20that%20allows%20investors%20to%20meet%20multiple%20managers%20in%20small%20LP%20groups.%20Our%20upcoming%20forum%20will%20be%20a%20virtual%20event.%20The%20event%20provides%20an%20efficient%20platform%20to%20meet%20other%20investors%20and%20managers%20over%20two%20half-day%20sessions.&location=Online&trp=true&sf=true&output=xml#f"
             >
               +GoogleCal
             </a>
