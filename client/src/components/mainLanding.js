167c6094ce6d8ba504d333cc90b10a8c6641cc69
Mitch Waite <mitchtwaite@gmail.com>
2021-02-24 16:10:21 -0700

    updated hero links

--- client/src/components/mainLanding.js
+++ client/src/components/mainLanding.js
@@ -10,8 +10,7 @@ const MainLanding = () => (
         <div className="hero-body">
           <div className="container">
             <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
-              <div className="column"></div>
-              <div className="column">
+              <div className="column is-half is-offset-6">
                 <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
                   Welcome to HUGO-LP Forums. We are a special invitation event
                   designed to bring together leading investors and asset
@@ -20,13 +19,23 @@ const MainLanding = () => (
               </div>
             </div>
             <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
-              <div className="column"></div>
-              <div className="column">
+              <div className="column is-half is-offset-6">
                 <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
-                  <Link style={{ color: 'orange' }} to="/form">
-                    Register
-                  </Link>{' '}
-                  for upcoming{' '}
+                  <u>Upcoming event:</u> HUGO 2021 Virtual Forum
+                </h2>
+              </div>
+            </div>
+            <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+              <div className="column is-half is-offset-6">
+                <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
+                  <u>Date:</u> April 28-29, 2021
+                </h2>
+              </div>
+            </div>
+            <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+              <div className="column is-half is-offset-6">
+                <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
+                  <u>Meeting info:</u>{' '}
                   <button type="button" className="pdf-download">
                     <a
                       className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile pdf-download"
@@ -35,10 +44,21 @@ const MainLanding = () => (
                       rel="noopener noreferrer"
                       href={pdf}
                     >
-                      <strong>forum</strong>
+                      Agenda
                     </a>
                   </button>
-                  : Virtual Event on April 28-29, 2021
+                </h2>
+              </div>
+            </div>
+            <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+              <div className="column is-half is-offset-6">
+                <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
+                  <u>Register now:</u>{' '}
+                  <h2 className="custom-h2 is-size-4-fullhd is-size-4-widescreen is-size-5-desktop is-size-5-touch is-size-6-tablet is-size-6-mobile">
+                    <Link style={{ color: 'orange' }} to="/form/virtual-forum">
+                      REGISTRATION
+                    </Link>{' '}
+                  </h2>
                 </h2>
               </div>
             </div>
